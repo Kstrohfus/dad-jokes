7a574882afd298a7c2345ed5fd9baba93327db8b
Kstrohfus <houston11@me.com>
2022-11-12 20:19:28 -0700

    Removed comment in js

--- js/script.js
+++ js/script.js
@@ -18,17 +18,5 @@ async function generateJoke() {
     }
 
     
-// function generateJoke() {
-//     const config = {
-//         headers: {
-//             Accept: "application/json",
-//         },
-//     }
-//     fetch("https://icanhazdadjoke.com", config).then((res)=> res.json())
-//     .then((data) => {
-//         jokeEl.innerHTML = data.joke
-//     })
-//     }
-    
 
 
